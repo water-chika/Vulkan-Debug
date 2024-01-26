557ae9d7b67d40cecec0b0a2011313c12e33da5a
Water Chika <Water Chika>
2024-01-26 01:19:31 -0800

    delete some ugly codes.

--- cached_call.cpp
+++ cached_call.cpp
@@ -1,61 +1,29 @@
 #include <map>
 #include <iostream>
 #include <chrono>
-
-int f_cached(int n);
-int f_uncached(int n);
-
-int f(int n) {
-	if (n <= 0) {
-		return 1;
-	}
-	return f(n - 1) + f(n - 2);
-}
-
-template<class F>
-int f(int n, F f_) {
-	if (n <= 0) {
-		return 1;
-	}
-	return f_(n - 1) + f_(n - 2);
-}
-
-int f_uncached(int n) {
-	if (n <= 0) {
-		return 1;
-	}
-	return f_cached(n - 1) + f_cached(n - 2);
-}
-
-int f_cached(int n) {
-	static std::map<int, int> cache;
-	int res;
-	if (cache.count(n) > 0) {
-		res = cache[n];
-	}
-	else {
-		res = f_uncached(n);
-		cache.emplace(n, res);
-	}
-	return res;
-}
+#include <functional>
 
 template<class F, class... Args>
-auto cached_call(F fun, Args... args) -> decltype(fun(args...)){
+class cached_caller {
 	using ret_type = decltype(fun(args...));
 	using index_type = std::tuple<Args...>;
-	static std::map<index_type, ret_type> cache;
-	ret_type res;
-	index_type index{ args... };
-	if (cache.count(index) > 0) {
-		res = cache[index];
+
+	ret_type operator()(Args... args) {
+		ret_type res;
+		index_type index{ args... };
+		if (m_cache.count(index) > 0) {
+			res = m_cache[index];
+		}
+		else {
+			res = fun(args...);
+			m_cache.emplace(index, res);
+		}
+		return res;
 	}
-	else {
-		res = fun(args...);
-		cache.emplace(index, res);
-	}
-	return res;
-}
+private:
+	static std::map<index_type, ret_type> m_cache;
+};
+
 
 template<class F, class... Args>
 requires std::invocable<F, Args...>
@@ -69,8 +37,5 @@ auto measure_duration(F f, Args... args) {
 }
 
 int main() {
-	std::cout << measure_duration(f, 100) << std::endl;
-	std::cout << measure_duration(f_uncached, 100) << std::endl;
-	std::cout << measure_duration(f_cached, 100) << std::endl;
 	return 0;
 }
